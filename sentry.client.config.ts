c7408b9ccccf64a01126967620d559081abdcd40
unclebabykern <coren.frankel@gmail.com>
2024-10-12 14:40:27 -0600

    chore: initialize sentry sdk/wizard for nextjs

---
+++ sentry.client.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://9f610d32b963dfceb8252f9419ea6f75@o4507986835275776.ingest.us.sentry.io/4508111636725760",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
